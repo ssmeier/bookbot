11fb4ce788f133e23cc770b4fe2326c459c66d85
ssmeier <ssmeier1@gmail.com>
2025-12-25 18:24:19 -0500

    finished data processing methods

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_letter_count
 
 def get_book_text (path_to_book):
     with open(path_to_book) as book:
@@ -7,5 +8,5 @@ def get_book_text (path_to_book):
 
 def main():
     print(get_num_words(get_book_text("books/frankenstein.txt")))
-
+    print(get_letter_count(get_book_text("books/frankenstein.txt")))
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@ def get_num_words (input_text):
     wordlist = input_text.split()
     wordcount = len(wordlist)
     return "Found "+ str(wordcount) +" total words"
+
+def get_letter_count (input_text):
+    letter_dict = {}
+    for char in input_text:
+        char = char.lower()
+        if char in letter_dict:
+            letter_dict[char] = (letter_dict.get(char) + 1)
+        else:
+            letter_dict[char] = 1
+
+    return letter_dict
